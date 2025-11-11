ca86bba185ffffea125fda0daa558ba541b39aa0
James-Lu-none <jamesabcde277@gmail.com>
2025-11-11 21:54:56 +0800

    fix reserved syntax using

--- preprocess.py
+++ preprocess.py
@@ -16,7 +16,7 @@ short_noises_path = os.path.join(BASE, "short_noises")
 rir_path = os.path.join(BASE, "rir")
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--type", type=str, required=True)
+parser.add_argument("--aug_type", type=str, required=True)
 parser.add_argument("--src_dir", type=str, required=True)
 parser.add_argument("--label_file", type=str, required=True)
 parser.add_argument("--dist_dir", type=str, required=True)
@@ -29,16 +29,16 @@ OutPath = os.path.join(BASE, args.dist_dir)
 OutLabelPath = os.path.join(OutPath, "metadata.csv")
 os.makedirs(OutPath, exist_ok=True)
 
-print("generating augmented data for type:", args.type)
+print("generating augmented data for type:", args.aug_type)
 print("input path:", InPath)
 print("input label file:", InLabelPath)
 print("output path:", OutPath)
 print("output label file:", OutLabelPath)
 print("number of augmentations per file:", args.n_augmentations)
 
-if args.type not in ["TPG", "TPGB", "TPGBIR"]:
+if args.aug_type not in ["TPG", "TPGB", "TPGBIR"]:
     raise ValueError("Invalid dataset name. Choose from: TPG, TPGB, TPGBIR")
-os.system(f"rm -rf ./data/train/train/{args.type}")
+os.system(f"rm -rf ./data/train/train/{args.aug_type}")
     
 sr = 16000
 
@@ -112,7 +112,7 @@ for file in tqdm(files, desc="Processing files", unit="file"):
         # augment1 may return a list; ensure we pass a single array to augment2
         src = augmented_samples1[0] if isinstance(augmented_samples1, (list, tuple)) else augmented_samples1
         augment2 = None
-        match args.type:
+        match args.aug_type:
             case "TPG":
                 augment2 = augment_TPG
             case "TPGB":
